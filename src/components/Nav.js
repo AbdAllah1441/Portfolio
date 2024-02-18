af600b7fdc03a90053838fe2dec1f2316212c135
عبدالله مصطفى <abdullahmostafa9957@gmail.com>
2024-02-18 23:34:13 +0200

    responsive

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,12 +2,14 @@ import React, { useState } from 'react'
 import './Nav.scss'
 import { Link } from 'react-router-dom'
 import code from "../images/code-solid.svg";
+import bars from "../images/bars-solid.svg";
 
 const Nav = () => {
     const [homeSelect, setHomeSelect] = useState(true);
     const [projectsSelect, setProjectsSelect] = useState(false);
     const [contactSelect, setContactSelect] = useState(false);
     const [skillsSelect, setSkillsSelect] = useState(false);
+    const [clickMenu, setClickMenu] = useState(false);
 
     const clicked = {
         home: [homeSelect, setHomeSelect],
@@ -21,20 +23,25 @@ const Nav = () => {
             clicked[key][1](false);
         }
         clicked[selected][1](true);
+        setClickMenu(false);
     };
 
     return (
         <div className='nav' >
             <Link to="/Portfolio/" className={homeSelect ? "logo selected-logo" : "logo"} 
             onClick={() => handleSelect("home")}><img src={code} alt='aa' /></Link>
-            <div className='sections'>
-                <Link className={`section ${homeSelect ? "selected" : ""}`} to="/Portfolio/" 
+            <div className={`sections ${clickMenu? "clicked" : ""}`}>
+                <div className="section">
+                    <img src={bars} 
+                    onClick={() => clickMenu? setClickMenu(false) : setClickMenu(true)} alt="" />
+                </div>
+                <Link className={`menu section ${homeSelect ? "selected" : ""}`} to="/Portfolio/" 
                 onClick={() => handleSelect("home")}>Home</Link>
-                <Link className={`section ${skillsSelect ? "selected" : ""}`} to="/skills" 
+                <Link className={`menu section ${skillsSelect ? "selected" : ""}`} to="/skills" 
                 onClick={() => handleSelect("skills")}>Skills</Link>
-                <Link className={`section ${projectsSelect ? "selected" : ""}`} to="/projects" 
+                <Link className={`menu section ${projectsSelect ? "selected" : ""}`} to="/projects" 
                 onClick={() => handleSelect("projects")}>Projects</Link>
-                <Link className={`section ${contactSelect ? "selected" : ""}`} to="/contact" 
+                <Link className={`menu section ${contactSelect ? "selected" : ""}`} to="/contact" 
                 onClick={() => handleSelect("contact")}>Contact</Link>
             </div>
         </div>
